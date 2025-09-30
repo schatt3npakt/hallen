ab8dd0a879768bd2fffdf29c28c5df1472b1dcbb
Luca La Furia <dev@bitheart.de>
2025-10-01 00:45:59 +0200

    feat: remove main menu

--- src/core/js/classes/Router.js
+++ src/core/js/classes/Router.js
@@ -8,7 +8,6 @@ export class Router {
   async init() {
     this.routes = {
       game: await loadHtml("./core/html/views/game.html"),
-      mainmenu: await loadHtml("./core/html/views/mainmenu.html"),
       options: await loadHtml("./core/html/views/options.html"),
       saves: await loadHtml("./core/html/views/saves.html"),
       title: await loadHtml("./core/html/views/title.html"),
